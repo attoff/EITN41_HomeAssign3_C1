d74ebcb3430ab5f7e69c262944fa2ccd622d9cb3
astrimagnussen <dic12ama@student.lu.se>
2016-12-02 21:52:31 +0100

    breac and conceal implemented

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,16 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * Created by vikto on 2016-12-02.
- */
 public class Main {
 
     public static void main(String[] args) {
 
         int k = 16;
-        int X = 160;
 
         String[] vYes = new String[(int) Math.pow(2, k + 1)];
         String[] vNo = new String[(int) Math.pow(2, k + 1)];
 
         MessageDigest md;
-
 
         for (int i = 0; i < vYes.length; i++) {
 
@@ -30,33 +25,39 @@ public class Main {
 
                 md.update(("0" + bin16).getBytes("UTF-8"));
                 String tmp = String.format("%064x", new java.math.BigInteger(1, md.digest()));
-                vYes[i] = tmp.substring(0, 8);
+                vYes[i] = tmp.substring(0, 1);
 
                 md.update(("1" + bin16).getBytes("UTF-8"));
                 tmp = String.format("%064x", new java.math.BigInteger(1, md.digest()));
-                vNo[i] = tmp.substring(0, 8);
+                vNo[i] = tmp.substring(0, 1);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
-        int prob = 0;
-        int g = 0;
-
+        int p = 0;
+        int q = 0;
+        int bind = 0;
+        int conc = 0;
         for (int i = 0; i < vYes.length; i++) {
-            innerloop:
+            p = 0;
+            q = 0;
             for (int j = 0; j < vNo.length; j++) {
-                if (vYes[i].compareTo(vNo[j]) == 0) {
-                    prob++;
-                    break innerloop;
+                if (vYes[i].compareTo(vNo[j]) == 0 && p == 0) {
+                    bind++;
+                    p = 1;
+                } else if (vNo[i].compareTo(vYes[j]) == 0 && q == 0) {
+                    conc++;
+                    q = 1;
                 }
             }
-            g++;
         }
-        System.out.println(g);
-        System.out.println(prob);
-        System.out.println(prob * 100 / vYes.length + "% probability");
-
+        float bindVal = (bind + conc) * 100 / (2 * vYes.length);
+        float conVal = (2 * vYes.length - (bind + conc)) * 100 / (2 * vYes.length);
+        System.out.println(bind + " found collisions out of " + vYes.length + " possible. This gives us a:");
+        System.out.println(bindVal + " % binding probability");
+        System.out.println(conc);
+        System.out.println(2 * vYes.length - (bind + conc) + " found values without collision");
+        System.out.println(conVal + " % concealig probability");
     }
 }
